3f4456bafb59664af9c9053b16eed9e23ea1ac00
lingalaabhi34 <lingalaabhi34@gmail.com>
2024-02-24 00:23:13 +0530

    submitting code-Sat Feb 24 2024 00:23:10 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,11 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key='relativeList'>
+                <li key='relativeListItem1'> relativeListItem1</li>
+                <li key='relativeListItem2'> relativeListItem2</li>
+                <li key='relativeListItem3'> relativeListItem3</li>
+               </ol>
     </div>
   )
 }
